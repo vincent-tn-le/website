b89c86ab0f658bee359a471ae20323c380a4b34b
Vincent Le <vincent.tn.le@gmail.com>
2024-03-05 16:30:31 -0500

    Added skills section

--- portfolio/src/components/Skills/skills.js
+++ portfolio/src/components/Skills/skills.js
@@ -11,26 +11,26 @@ const Skills = () => {
             <span className="skillDesc">A data engineer from the University of Waterloo with a proven track record of development, optimization, and management of robust data platforms. In my prior role as a data engineer, I have led the design and development of data pipelines, enhanced batch processing run times, and managed technical issues for data warehouses in Delta Lake and SQL databases. <br></br> As I seek new opportunities within software engineering focused on Data Platform, I am keen on leveraging my technical experience, problem solving skills, and novel thinking to drive value for an organization. I am eager to bring my adaptability and dedication to a mission-driven team that values forward-thinking solutions.</span>
             <div className="skillsBar">
                 <div className="skillBar">
-                    <img src="" alt="" className="skillBarImg"/>
+                    <img src={PythonLogo} alt="Python" className="skillBarImg"/>
                     <div className="skillBarText">
                         <h2>Python</h2>
-                        <p></p>
+                        <p>Sample text</p>
                     </div>
                 </div>
 
                 <div className="skillBar">
-                    <img src="" alt="" className="skillBarImg"/>
+                    <img src={SparkLogo} alt="Apache Spark" className="skillBarImg"/>
                         <div className="skillBarText">
                             <h2>Apache Spark</h2>
-                            <p></p>
+                            <p>Sample text</p>
                         </div>
                 </div>
 
                 <div className="skillBar">
-                    <img src="" alt="" className="skillBarImg"/>
+                    <img src={DatabricksLogo} alt="Databricks" className="skillBarImg"/>
                         <div className="skillBarText">
                             <h2>Databricks</h2>
-                            <p></p>
+                            <p>Sample text</p>
                         </div>
                 </div>
             </div>
