7f03487fc134f92f1a68d8439f5079890b38fc73
Vincent Le <vincent.tn.le@gmail.com>
2024-03-06 18:13:11 -0500

    Hosted projects and completed section

---
+++ portfolio/src/components/Contact/contact.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import contactImg from '../../assets/egress.png'
+import contactImg from '../../assets/wealthsimple.png'
+import contactImg from '../../assets/pwc.png'
+import contactImg from '../../assets/contact.png'
+
+const Contact = () => {
+    return (
+        <section className="contactPage">
+            <div id="photos">
+                <h1 className="contactPageTitle">Previous Experience</h1>
+                <p className="experienceDesc">I have had the opportunity to work with a diverse group of companies.
+                Some of them include:</p>
+                <div className="experienceImgs">
+                    <img src ={} alt="" className="experienceImg"></img>
+                    <img src ={} alt="" className="experienceImg"></img>
+                    <img src ={} alt="" className="experienceImg"></img>
+                    <img src ={} alt="" className="experienceImg"></img>
+                </div>
+
+            </div>
+            
+            <div id="contact">
+
+            </div>
+        </section>
+    )
+}
--- portfolio/src/components/Projects/projects.js
+++ portfolio/src/components/Projects/projects.js
@@ -12,37 +12,38 @@ const Projects = () => {
             <h2 className="projectsTitle">Projects</h2>
             <span className="projectsDesc">Some of the projects that I have completed over the years.</span>
             <div className="projectImgs">
-                <div className="projectImg">
+                <a href="https://vincent-tn-le.github.io/art-supply-scraper/"><div className="projectImg">
                     <img src={Artscraper} alt="Artscraper" className="projectBarImg"/>
                         <div className="projectBarText">
                             <h2>Web Scraper</h2>
                             <p>Python (BeautifulSoup, Flask), PostgreSQL, React.js</p>
                         </div>
-                </div>
+                </div></a>
 
-                <div className="projectImg">
+                <a href="https://vincent-tn-le.github.io/etch-a-sketch/"><div className="projectImg">
                     <img src={Etch} alt="Etch-a-Sketch" className="projectBarImg"/>
                         <div className="projectBarText">
                             <h2>Etch-a-Sketch</h2>
                             <p>JavaScript, HTML, CSS</p>
                         </div>
-                </div>
+                </div></a>
 
-                <div className="projectImg">
+                <a href="https://github.com/vincent-tn-le/stock_trading"><div className="projectImg">
                     <img src={Stock} alt="Stock trading bot" className="projectBarImg"/>
                         <div className="projectBarText">
                             <h2>Forex Trading Bot</h2>
                             <p>Python, FXCM API Connection, Pandas</p>
                         </div>
                 </div>
+                </a>
 
-                <div className="projectImg">
+                <a href="https://github.com/vincent-tn-le/classfying_music_genres"><div className="projectImg">
                     <img src={Music} alt="Classifying Music Genres" className="projectBarImg"/>
                         <div className="projectBarText">
                             <h2>Music Genre Classifier</h2>
                             <p>Python, Scikit-learn, Jupyter Notebook</p>
                         </div>
-                </div>
+                </div></a>
 
             </div>
         </section>
