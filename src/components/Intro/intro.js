00d77d3a5d3fe3ef10e4a5ee2e9dbe5683aae7ac
Vincent Le <vincent.tn.le@gmail.com>
2024-03-10 13:54:01 -0400

    added resume link

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -10,7 +10,7 @@ const Intro = () => {
                 <span className="nameTitle">Vincent Le</span>
                 <span className="education">Statistics @ University of Waterloo 2020</span>
                 <span className="details">Programmer, Photographer, Chef</span><br></br> 
-                <a href="../../assets/vincent-le_resume.pdf" target="_blank"><button className="resumeButton"><img src={btnImg} className="btnImg"></img> Resume</button></a>
+                <a href="https://github.com/vincent-tn-le/website/blob/main/src/assets/vincent-le_resume.pdf" target="_blank"><button className="resumeButton"><img src={btnImg} className="btnImg"></img> Resume</button></a>
             </div>
             
         </section>
